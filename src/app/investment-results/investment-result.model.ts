e16f4f0bc9af4021dab02650ed282f5489bd84b7
Andrés Jiménez <andresito_s2@live.com>
2024-07-22 13:59:47 -0500

    Add changes

--- src/app/investment-results/investment-result.model.ts
+++
@@ -1,8 +0,0 @@
-export interface results { 
-      year: number,
-      interest: number,
-      valueEndOfYear: number,
-      annualInvestment: number,
-      totalInterest: number,
-      totalAmountInvested: number,
-}
--- src/app/user-input/user-input.model.ts
+++
@@ -1,7 +0,0 @@
-export interface userInput {
-  initialInvestment: number,
-  annualInvestment: number,
-  expectedReturn: number,
-  duration: number,
-
-}
